0a8e047a4ca5e12fd8674dbf89c0faba15c23981
Tomasskuta <tomasskuta@seznam.cz>
2024-05-31 01:32:42 +0200

    Update texts.py

--- texts.py
+++ texts.py
@@ -10,9 +10,9 @@ TEXTS = [
             much steeper buff-to-white beds of the Green River Formation, which are about 300 feet
             thick.""",
 
-    """The monument contains 8198 acres and protects a portion of the largest deposit of freshwater fish
+        """The monument contains 8198 acres and protects a portion of the largest deposit of freshwater fish
             fossils in the world. The richest fossil fish deposits are found in multiple limestone layers,
             which lie some 100 feet below the top of the butte. The fossils represent several varieties
             of perch, as well as other freshwater genera and herring similar to those in modern oceans.
-            Other fish such as paddlefish, garpike and stingray are also present."""
+            Other fish such as paddlefish, garpike and stingray are also present."""       
 ]
