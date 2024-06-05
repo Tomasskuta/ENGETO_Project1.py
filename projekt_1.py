83d7dde2591ba4efb790b313f3d301df480f05ab
Tomasskuta <tomasskuta@seznam.cz>
2024-06-05 15:14:37 +0200

    Update projekt_1.py
    notes

--- projekt_1.py
+++ projekt_1.py
@@ -4,8 +4,8 @@ author: Tomáš Škuta
 email: tomasskuta@seznam.cz
 discord: smajlikskutik
 """
-
-from texts import *
+# using files in case we would use .exe file so we can analyze any other text, same for users - those should be hashed iif used this way
+from texts import * 
 from users import *
 import pwinput
 
@@ -19,7 +19,7 @@ def main():
     if username in users and users[username] == password:
         print("--------------------------------------------------")
         print(f"Welcome to the app, {username}.") 
-        print(f"We have {str(len(TEXTS))} texts to be analyzed.")
+        print(f"We have {str(len(TEXTS))} texts to be analyzed.")   # using len of text if we have more/other texts in next usage, also in other cases below
         print("--------------------------------------------------")
 
         try:
@@ -32,9 +32,9 @@ def main():
             print("Invalid input, terminating the program..")
             return
         
-        selected_text = TEXTS[selection - 1]
+        selected_text = TEXTS[selection - 1].replace("-", " ")  # not sure if we had to replace buff-to-white with buff to white, depends on user requirements 
         words_split = selected_text.split()
-        words = [(word.strip(".,?!-%+/*()§\"")) for word in words_split]
+        words = [(word.strip(".,?!-%+/*()§\"")) for word in words_split]    # deleting those characters, as we dont need them and those could make some troubles while counting below
 
         words_count = len(words)
         print(f"There are {words_count} words in the selected text.")
@@ -54,11 +54,12 @@ def main():
         numerics_count = sum(int(word) for word in words if word.isnumeric())
         print(f"The sum of all the numbers is {numerics_count}.")
 
-        words_max_lenght = max([len(word) for word in words])
+        words_max_lenght = max([len(word) for word in words])   # getting maximum lenght of word in the text for the range of for cycle to getting max occurrence of words lenght
 
-        max_occurrence = max(sum(1 for word in words if len(word) == i) for i in range(1, words_max_lenght + 1))
-        max_occurrence_graph = str((int((max_occurrence-11)/2)) * " ")
+        max_occurrence = max(sum(1 for word in words if len(word) == i) for i in range(1, words_max_lenght + 1))    # getting maximum occurrence for "graph" to show exact lenght of maximum occurence (header wise) and also other lenghts wise
+        max_occurrence_graph = str((int((max_occurrence - 11)/2)) * " ")  # variable for lenght of the occurrence column in header
 
+        # this is fix of the graph lenght when max_occurence is even number
         if max_occurrence % 2 == 0:
             max_occurrence_graph2 = max_occurrence_graph + " "
         else:
